449932119e08a90a606e47365d2347005847988e
jumpre <skyblueyoshisaur@gmail.com>
2024-04-04 16:19:12 +0100

    Quick blank yellow screen graphical project.

--- main.py
+++ main.py
@@ -5,22 +5,35 @@ FPS = 60; WIDTH = 800; HEIGHT = 600
 ANTIALIASING = True
 
 testData0 = ["1. Introduction.", "2. About atoms.", "3. Molecules",
-	"4. Why you should care."]
+    "4. Why you should care."]
 
 def main():
-	print(getSlides.getSlides("testData/dogData.txt"))
+    #print(getSlides.getSlides("testData/dogData.txt"))
+    displayRun()
 
 def displayRun() -> None:
-	while True:
+    pygame.init()
+    window = pygame.display.set_mode((WIDTH,HEIGHT))
+    pygame.display.set_caption("client")
+    clock = pygame.time.Clock()
+    while True:
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        window.fill( (255, 255, 0) )
+        pygame.display.update()
+        clock.tick(FPS)
 
 def prototype():
-	currentIndex = 0
-	while True:
-		usrInput = input(testData0[currentIndex])
-		if usrInput == "quit":
-			quit()
-		currentIndex = (currentIndex + 1) % len(testData0)
+    currentIndex = 0
+    while True:
+        usrInput = input(testData0[currentIndex])
+        if usrInput == "quit":
+            quit()
+        currentIndex = (currentIndex + 1) % len(testData0)
 
 if __name__ == "__main__":
-	main()
+    main()
